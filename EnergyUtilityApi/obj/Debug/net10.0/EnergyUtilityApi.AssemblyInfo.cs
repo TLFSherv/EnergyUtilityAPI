8655b2d1d0816e9885f64e7e0d47b91df2e892c6
Sherwin Burgess <sherwinburgess@gmail.com>
2026-03-24 23:16:45 +0100

    api outline

--- EnergyUtilityApi/obj/Debug/net10.0/EnergyUtilityApi.AssemblyInfo.cs
+++ EnergyUtilityApi/obj/Debug/net10.0/EnergyUtilityApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EnergyUtilityApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed51e9e4a1493d946d3f91c536a0d6403bd498b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("EnergyUtilityApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EnergyUtilityApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
