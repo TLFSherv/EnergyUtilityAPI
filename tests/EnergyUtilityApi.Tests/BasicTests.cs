1ec1bb1b4193eb27d020f9f3bd779a53f4e179b5
Sherwin Burgess <sherwinburgess@gmail.com>
2026-04-04 00:18:18 +0100

    fix bugs with validation from unit testing

--- tests/EnergyUtilityApi.Tests/BasicTests.cs
+++ tests/EnergyUtilityApi.Tests/BasicTests.cs
@@ -1,6 +1,7 @@
 using Xunit;
 using Microsoft.AspNetCore.Mvc.Testing;
 using EnergyUtilityApi;
+using System.Net;
 namespace EnergyUtilityApi.Tests;
 
 public class BasicTests : IClassFixture<WebApplicationFactory<Program>>
@@ -13,10 +14,27 @@ public class BasicTests : IClassFixture<WebApplicationFactory<Program>>
     }
 
     [Fact]
+    public async Task GetEnergyCostScotland_ReturnsOkResult()
+    {
+        var client = _factory.CreateClient();
+        var response = await client.GetAsync("/api/energy-utility/cost?Postcode=AB10AG&PropertyType=6&PropertyAge=11&NumberOfAdults=3&FloorArea=4");
+        response.EnsureSuccessStatusCode();
+    }
+
+    [Fact]
     public async Task GetEnergyData_ReturnsOkResult()
     {
         var client = _factory.CreateClient();
-        var response = await client.GetAsync("/api/energy-utility/cost?Postcode=AB10AG&PrpertyType=4&PropertyAge=1&HouseholdSize=3");
+        var response = await client.GetAsync("/api/energy-utility?Postcode=B330AA");
         response.EnsureSuccessStatusCode();
     }
+
+    [Fact]
+    public async Task GetEnergyCost_BadRequest()
+    {
+        var client = _factory.CreateClient();
+        var response = await client.GetAsync("/api/energy-utility/cost?Postcode=A");
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+    }
+
 }
