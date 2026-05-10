8715fd37ab2ae028daecb698d1f4f27d81b688ad
Sherwin Burgess <sherwinburgess@gmail.com>
2026-05-10 17:25:06 +0100

    change app website for CORS

--- src/EnergyUtilityApi/Program.cs
+++ src/EnergyUtilityApi/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: MyAllowSpecificOrigins, policy =>
         {
-            policy.WithOrigins("http://localhost:5094*")
+            policy.WithOrigins("https://energyutilityapp.onrender.com*")
              .SetIsOriginAllowedToAllowWildcardSubdomains();
         });
 });
